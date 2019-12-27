f822fbcf22fc91e19d36a05a5a7ca161ff086a02
Admi-ral <37858521+Admi-ral@users.noreply.github.com>
2019-12-27 16:33:17 -0500

    1.0 General outline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {WallComponent} from './wall/wall.component';
+import {FriendsComponent} from './friends/friends.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo:'/wall', pathMatch:'full'},
+  {path:'wall', component: WallComponent},
+  {path:'friends', component: FriendsComponent},
+  {path: "**", component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+WallComponent,
+FriendsComponent,
+PageNotFoundComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
